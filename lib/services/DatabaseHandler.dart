4840d3c2aa68a1a40bf5021079cd6576f1e567e8
abunav6 <anubhavdinkar05@gmail.com>
2022-01-02 09:41:05 +0530

    fixed an issue where all data was being loaded for each title in the DB, thereby slowing down loading time

--- lib/services/DatabaseHandler.dart
+++ lib/services/DatabaseHandler.dart
@@ -1,15 +1,16 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:flutter/material.dart';
 import "package:mtvdb/helper.dart";
 
 Future<Database> initializeDB() async {
   String path = await getDatabasesPath();
-  //deleteDatabase(join(path, "watchd.db"));
+  // deleteDatabaser(join(path, "mtv.db"));
   return openDatabase(
-    join(path, 'watchd.db'),
+    join(path, 'mtv.db'),
     onCreate: (database, version) async {
       await database.execute(
-        "CREATE TABLE IF NOT EXISTS watchd(imdbID TEXT PRIMARY KEY, type TEXT NOT NULL, watchlist TEXT NOT NULL)",
+        "CREATE TABLE IF NOT EXISTS MTVDB(imdbID TEXT PRIMARY KEY, title TEXT NOT NULL, poster TEXT NOT NULL, type TEXT NOT NULL, watchlist TEXT NOT NULL)",
       );
     },
     version: 1,
@@ -18,27 +19,29 @@ Future<Database> initializeDB() async {
 
 Future<int> insert(Database db, Record rec) async {
   int result = 0;
-  result = await db.insert("watchd", rec.toMap());
+  result = await db.insert("MTVDB", rec.toMap());
   return result;
 }
 
 Future<List<Record>> retrieveAll(Database db) async {
-  final List<Map<String, Object?>> queryResult = await db.query('watchd');
+  final List<Map<String, Object?>> queryResult = await db.query('MTVDB');
   return queryResult.map((e) => Record.fromMap(e)).toList();
 }
 
 Future<List<Record>> retrieveData(
     Database db, String type, String watchlist) async {
-  final List<Map<String, Object?>> queryResult = await db.query('watchd',
+  debugPrint("starting retrieval");
+  final List<Map<String, Object?>> queryResult = await db.query('MTVDB',
       where: 'type=? AND watchlist=?', whereArgs: [type, watchlist]);
+  debugPrint("ending retrieval");
   return queryResult.map((e) => Record.fromMap(e)).toList();
 }
 
 void changeWatchlist(Database db, Record rec) async {
-  await db.update("watchd", rec.toMap(),
+  await db.update("MTVDB", rec.toMap(),
       where: "imdbID= ?", whereArgs: [rec.imdbID]);
 }
 
 void delete(Database db, String imdbID) async {
-  await db.delete("watchd", where: "imdbId=?", whereArgs: [imdbID]);
+  await db.delete("MTVDB", where: "imdbId=?", whereArgs: [imdbID]);
 }
