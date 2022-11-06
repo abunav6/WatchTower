5b8d546e0d39f6e5b51c3ae1dea86a513792c660
anubhavdinkar <anubhavdinkar05@gmail.com>
2022-11-06 21:14:07 +0530

    cleaned up ImageSearch class

--- lib/person_helper.dart
+++ lib/person_helper.dart
@@ -69,42 +69,17 @@ class ImageSearch {
 }
 
 class Profiles {
-  double? aspectRatio;
-  int? height;
-  Null? iso6391;
   String? filePath;
-  double? voteAverage;
-  int? voteCount;
-  int? width;
 
-  Profiles(
-      {this.aspectRatio,
-      this.height,
-      this.iso6391,
-      this.filePath,
-      this.voteAverage,
-      this.voteCount,
-      this.width});
+  Profiles({this.filePath});
 
   Profiles.fromJson(Map<String, dynamic> json) {
-    aspectRatio = json['aspect_ratio'];
-    height = json['height'];
-    iso6391 = json['iso_639_1'];
     filePath = json['file_path'];
-    voteAverage = json['vote_average'];
-    voteCount = json['vote_count'];
-    width = json['width'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['aspect_ratio'] = this.aspectRatio;
-    data['height'] = this.height;
-    data['iso_639_1'] = this.iso6391;
     data['file_path'] = this.filePath;
-    data['vote_average'] = this.voteAverage;
-    data['vote_count'] = this.voteCount;
-    data['width'] = this.width;
     return data;
   }
 }
