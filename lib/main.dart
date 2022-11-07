90678905a9d97adcc5831ec0e558535b58086883
anubhavdinkar <anubhavdinkar05@gmail.com>
2022-11-07 14:45:29 +0530

    cleanup /tmp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import "./signedin.dart";
 
 void main() async {
@@ -10,11 +13,27 @@ class WatchD extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    _cleanUpTemporaryDirectory();
     return MaterialApp(
         title: 'WatchD',
         theme: ThemeData(
             primarySwatch: Colors.blue, unselectedWidgetColor: Colors.white),
         initialRoute: "/",
         routes: {"/": (context) => const SignedInWidget()});
+  }
+
+  void _cleanUpTemporaryDirectory() async {
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    documentsDirectory.parent.list().forEach((child) async {
+      if (child is Directory && child.path.endsWith('/tmp')) {
+        debugPrint('Deleting temp folder at ${child.path}...');
+        try {
+          await child.delete(recursive: true);
+          debugPrint('Temp folder was deleted with success');
+        } catch (error) {
+          debugPrint('Temp folder could not be deleted: $error');
+        }
+      }
+    });
   }
 }
