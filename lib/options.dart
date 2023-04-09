4295ed5f8bddca9a4d0d64ebc6a9373dffd79fb8
anubhavdinkar <anubhavdinkar05@gmail.com>
2023-04-09 14:04:42 +0530

    converted OptionsScreen to Stateful

--- lib/options.dart
+++ lib/options.dart
@@ -1,17 +1,22 @@
-// ignore_for_file: use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 import 'package:mtvdb/details.dart';
 import "helper.dart";
 
-class OptionsScreen extends StatelessWidget {
+class OptionsScreen extends StatefulWidget {
   // Declare a field that holds the Person data
   final List<SearchDetails> options;
 
+  const OptionsScreen({Key? key, required this.options}) : super(key: key);
+
+  @override
+  _OptionsScreen createState() => _OptionsScreen();
+}
+
+class _OptionsScreen extends State<OptionsScreen> {
   final ScrollController controller = ScrollController();
-
-  OptionsScreen({Key? key, required this.options}) : super(key: key);
-
+  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,46 +24,62 @@ class OptionsScreen extends StatelessWidget {
         appBar: AppBar(backgroundColor: Colors.black),
         body: Padding(
             padding: const EdgeInsetsDirectional.fromSTEB(5, 30, 5, 30),
-            child: ListView.separated(
-                separatorBuilder: (BuildContext context, int index) {
-                  return const SizedBox(
-                    height: 10,
-                  );
-                },
-                itemCount: options.length,
-                physics: const AlwaysScrollableScrollPhysics(),
-                controller: controller,
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                      onTap: () async {
-                        TitleDetails title =
-                            await getDetails(options[index].imdbID);
-
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => DetailsScreenWidget(
-                                      title: title,
-                                      showButtons: true,
-                                    )));
+            child: Stack(children: [
+              Opacity(
+                  opacity: isLoading ? 0.5 : 1,
+                  child: ListView.separated(
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const SizedBox(
+                          height: 10,
+                        );
                       },
-                      child: Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0)),
-                          child: Padding(
-                              padding: const EdgeInsetsDirectional.fromSTEB(
-                                  5, 30, 5, 30),
-                              child: ListTile(
-                                leading: CircleAvatar(
-                                    radius: 30,
-                                    backgroundImage:
-                                        NetworkImage(options[index].poster)),
-                                title: Text(
-                                    "${options[index].title} (${options[index].year})"),
-                                trailing: const Icon(Icons.arrow_right),
-                              ))));
-                })));
+                      itemCount: widget.options.length,
+                      physics: const AlwaysScrollableScrollPhysics(),
+                      controller: controller,
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return GestureDetector(
+                            onTap: () async {
+                              setState(() {
+                                isLoading = true;
+                              });
+
+                              TitleDetails title = await getDetails(
+                                  widget.options[index].imdbID);
+                              setState(() {
+                                isLoading = false;
+                              });
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => DetailsScreenWidget(
+                                            title: title,
+                                            showButtons: true,
+                                          )));
+                            },
+                            child: Card(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30.0)),
+                                child: Padding(
+                                    padding:
+                                        const EdgeInsetsDirectional.fromSTEB(
+                                            5, 30, 5, 30),
+                                    child: ListTile(
+                                      leading: CircleAvatar(
+                                          radius: 30,
+                                          backgroundImage: NetworkImage(
+                                              widget.options[index].poster)),
+                                      title: Text(
+                                          "${widget.options[index].title} (${widget.options[index].year})"),
+                                      trailing: const Icon(Icons.arrow_right),
+                                    ))));
+                      })),
+              if (isLoading)
+                const Center(
+                  child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white)),
+                )
+            ])));
   }
 }
